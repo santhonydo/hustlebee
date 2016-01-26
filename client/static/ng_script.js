405912fb4af5b7f404817a8f021b8b751033d635
Anthony Do <santhonydo@gmail.com>
2016-01-26 12:23:28 -0800

    loginreg

--- client/static/ng_script.js
+++ client/static/ng_script.js
@@ -9,6 +9,30 @@ hustleBeeAppModule.config(function($stateProvider, $urlRouterProvider){
 			templateUrl: '/static/partials/homepage.html',
 			controller: 'HomepageController'
 		})
+
+		.state('posting_job', {
+			url: '/posting_job',
+			templateUrl: '/static/partials/posting_job.html',
+			controller: 'PostingJobController'
+		})
+
+		.state('logIn', {
+			url: '/log_in',
+			controller: 'LogInController',
+			templateUrl: '/static/partials/log_in.html',
+			params: {
+				obj: null
+			}
+		})
+
+		.state('registration', {
+			url: '/registration',
+			controller: 'RegistrationController',
+			templateUrl: '/static/partials/registration.html',
+			params: {
+				obj: null
+			}
+		})
 });
 
 hustleBeeAppModule.factory('hustleBeeAppFactory', function($http){
@@ -18,12 +42,13 @@ hustleBeeAppModule.factory('hustleBeeAppFactory', function($http){
 		$http.post('/signUp', data).success(function(success){
 			callback(success);
 		})
-	}
+	};
 
 	return factory;
 });
 
-hustleBeeAppModule.controller('HomepageController', function($scope, $location, hustleBeeAppFactory){
+hustleBeeAppModule.controller('HomepageController', function($scope, $location, $uibModal, $state, $stateParams, hustleBeeAppFactory){
+	console.log("in HomepageController")
 	$scope.emailSignUp = function(signupForm){
 		if(angular.isUndefined($scope.signup)){
 			$scope.error = "Opps! Did you forget to enter all your information?";
@@ -44,6 +69,52 @@ hustleBeeAppModule.controller('HomepageController', function($scope, $location, 
 			$scope.error = "";
 			$scope.success = "See-Bee-See. Thanks for signing up!";
 		}	
+	};
+
+	$scope.open = function() {
+		var modalInstance = $uibModal.open({
+    		animation: true,
+      		templateUrl: '/static/partials/job_posting_modal.html',
+      		controller: 'JobPostingModalController',
+    	});
 	}
 
+	
+
+	
 });
+
+hustleBeeAppModule.controller('JobPostingModalController', function($scope, $location, $state, $stateParams, $uibModalInstance) {
+	
+	$scope.close = function(){
+    	$uibModalInstance.dismiss('cancel');
+  	};
+
+	$scope.jobPosition = ["Outpatient Pharmacist", "Inpatient Pharmacist", "Pharmacy Technician", "Pharmacy Clerk"]
+
+	$scope.postJob = function(postingJob) {
+		if((angular.isUndefined($scope.postingJob.name) || angular.isUndefined($scope.postingJob.jobPosition) || angular.isUndefined($scope.postingJob.jobDescription)) || ($scope.postingJob.name == "" || $scope.postingJob.jobPostion == "" || $scope.postingJob.jobDescription == "")){
+			$scope.error = "Please enter all fields";
+		}else {
+			$state.go('logIn', {obj: $scope.postingJob});
+			$uibModalInstance.dismiss();
+			$scope.postingJob = {};
+			$scope.error ="";
+		}
+	}
+});
+
+hustleBeeAppModule.controller('LogInController', function($scope, $location, $state, $stateParams, hustleBeeAppFactory){
+	
+	$scope.registration = function() {
+		$state.go('registration', $stateParams.obj)
+	}
+})
+
+hustleBeeAppModule.controller('RegistrationController', function($scope, $location, $state, $stateParams, hustleBeeAppFactory){
+	$scope.error = ""
+
+	// $scope.
+})
+
+
