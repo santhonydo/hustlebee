5f44b6db1570f2bdfc56301b41f4dc2a586c4c16
Anthony Do <santhonydo@gmail.com>
2016-02-07 21:44:03 -0800

    debug register modal

--- client/static/ng_script.js
+++ client/static/ng_script.js
@@ -316,6 +316,10 @@ hustleBeeAppModule.controller('AuthController', function($scope, $rootScope, $ui
 	}
 
 	$scope.register = function(newUser){
+		if(angular.isUndefined(newUser)){
+			$scope.error = true;
+			$scope.errorMessage = "All fields are required.";
+		} else
 		if (angular.isUndefined(newUser.firstName) || angular.isUndefined(newUser.lastName) || angular.isUndefined(newUser.companyName) || angular.isUndefined(newUser.email) || angular.isUndefined(newUser.username) || angular.isUndefined(newUser.password)){
 			$scope.error = true;
 			$scope.errorMessage = "All fields are required.";
