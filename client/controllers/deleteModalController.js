146513c2fc94a62c95641cd2d05de5de0855bb9a
ericheinmiller <ericheinmiller@gmail.com>
2016-08-20 18:42:35 -0700

    fixed admin delete by injecting informaiton facotry into controller

--- client/controllers/deleteModalController.js
+++ client/controllers/deleteModalController.js
@@ -1,4 +1,4 @@
-hustleBeeAppModule.controller('deleteModalController', function($scope, $rootScope, $uibModal, $uibModalStack, $uibModalInstance, userDeleteInfo, $location, $state, $stateParams, hustleBeeAppFactory){
+hustleBeeAppModule.controller('deleteModalController', function($scope, $rootScope, $uibModal, $uibModalStack, $uibModalInstance, userDeleteInfo, $location, $state, $stateParams, informationFactory, hustleBeeAppFactory){
 
   $scope.error = false;
   $scope.disabled = true;
